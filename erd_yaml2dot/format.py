15cdfe4a2b8b32dba22631c84a53801d09d16851
Michaël Roynard <michaelroynard@gmail.com>
2023-05-05 18:50:41 +0200

    Fix formating routines depending on tests

--- erd_yaml2dot/format.py
+++ erd_yaml2dot/format.py
@@ -5,71 +5,78 @@ def rchop(s, suffix):
   return s
 
 
-def format_label_entity_for_dot(entity_name, entity_content, html=True, with_pk=False):
-  if html:
-    label = "    <table border='0' cellborder='0' cellspacing='0'>\n"
-    label += "      <tr><td bgcolor='darkgrey'><b><u>{}</u></b></td></tr>\n".format(
-        entity_name)
-    for field in entity_content['fields']:
-      if with_pk and 'primary key' in entity_content and field in entity_content['primary key']:
-        label += "      <tr><td align='left'><b>{}</b></td></tr>\n".format(
-            field)
-      else:
-        label += "      <tr><td align='left'>{}<br/></td></tr>\n".format(
-            field)
-    label += "    </table>"
-
-    # return '"{}" [label=<\n{}\n  >];'.format(entity_name, label)
-    return label
-
-  else:
-    max_size = max([len(field) for field in entity_content['fields']])
-    dashes = '-' * (max_size + 2)
-    label = '{}\\n{}\\n'.format(entity_name, dashes)
-    for field in entity_content['fields']:
-      if with_pk and 'primary key' in entity_content and field in entity_content['primary key']:
-        label += '*{}*\\n'.format(field)
-      else:
-        label += '{}\\n'.format(field)
-    # trim last \n
-    label = rchop(label, '\\n')
-
-    # return '"{}" [label="{}"];'.format(entity_name, label)
-    return label
-
-
-def format_label_relationship_for_dot(rel_name, rel_dict, html=True, with_pk=False):
-  if html:
-    label = "    <table border='0' cellborder='0' cellspacing='0'>\n"
-    label += "      <tr><td bgcolor='darkgrey'><b><u>{}</u></b></td></tr>\n".format(
-        rel_name)
-    for field in rel_dict['fields']:
-      if with_pk and 'primary key' in rel_dict and field in rel_dict['primary key']:
-        label += "      <tr><td align='left'><b>{}</b></td></tr>\n".format(
-            field)
-      else:
-        label += "      <tr><td align='left'>{}<br/></td></tr>\n".format(
-            field)
-    label += "    </table>"
-
-    # return '"{}" [label=<\n{}\n  >];'.format(rel_name, label)
-    return label
-
-  else:
-    max_size = max([len(field) for field in rel_dict['fields']])
-    dashes = '-' * (max_size + 2)
-    label = '{}\\n{}\\n'.format(rel_name, dashes)
-    for field in rel_dict['fields']:
-      if with_pk and 'primary key' in rel_dict and field in rel_dict['primary key']:
-        label += '*{}*\\n'.format(field)
-      else:
-        label += '{}\\n'.format(field)
-    # trim last \n
-    label = rchop(label, '\\n')
-
-    # return '"{}" [label="{}"];'.format(rel_name, label)
-    return label
-
-
 def format_card(min, max):
   return "{}..{}".format(min, max)
+
+
+def format_text_data_html(text, style):
+  html = "<td bgcolor='{}' align='{}'>".format(style['fillcolor'], style['align'])
+  html += "<font color='{}' face='{}' point-size='{}'>".format(style['color'], style['fontname'], style['fontsize'])
+  if style['bold']:
+    html += "<b>"
+  if style['underlined']:
+    html += "<u>"
+  if style['italic']:
+    html += "<i>"
+  html += text
+  if style['italic']:
+    html += "</i>"
+  if style['underlined']:
+    html += "</u>"
+  if style['bold']:
+    html += "</b>"
+
+  html += "</font></td>"
+  return html
+
+
+def format_label_entity_for_dot_text(entity_name, entity_content):
+  max_size = max([len(field) for field in entity_content['fields']])
+  dashes = '-' * (max_size + 2)
+  label = '{}\\n{}\\n'.format(entity_name, dashes)
+  for field in entity_content['fields']:
+    if 'primary_key' in entity_content and field in entity_content['primary_key']:
+      label += '*{}*\\n'.format(field)
+    else:
+      label += '{}\\n'.format(field)
+  # trim last \n
+  label = rchop(label, '\\n')
+
+  return label
+
+
+def format_label_relationship_for_dot_text(relationship_name, relationship_content):
+  max_size = max([len(field) for field in relationship_content['fields']])
+  dashes = '-' * (max_size + 2)
+  label = '{}\\n{}\\n'.format(relationship_name, dashes)
+  for field in relationship_content['fields']:
+    label += '{}\\n'.format(field)
+  # trim last \n
+  label = rchop(label, '\\n')
+
+  return label
+
+
+def format_label_entity_for_dot_html(entity_name, entity_content, style):
+  label = "    <table border='0' cellborder='0' cellspacing='0'>\n"
+  label += "      <tr>{}</tr>\n".format(format_text_data_html(entity_name, style['title']))
+  if 'fields' in entity_content:
+    for field in entity_content['fields']:
+      if 'primary_key' in entity_content and field in entity_content['primary_key']:
+        label += "      <tr>{}</tr>\n".format(format_text_data_html(field, style['primary_key']))
+      else:
+        label += "      <tr>{}</tr>\n".format(format_text_data_html(field, style['field']))
+  label += "    </table>"
+
+  return label
+
+
+def format_label_relationship_for_dot_html(relationship_name, relationship_content, style):
+  label = "    <table border='0' cellborder='0' cellspacing='0'>\n"
+  label += "      <tr>{}</tr>\n".format(format_text_data_html(relationship_name, style['title']))
+  if 'fields' in relationship_content:
+    for field in relationship_content['fields']:
+      label += "      <tr>{}</tr>\n".format(format_text_data_html(field, style['field']))
+  label += "    </table>"
+
+  return label
